6989101e0c603fa839caed394157da586fd74bab
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2022-08-18 11:21:54 -0400

    add example folder

---
+++ example/example.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+
+int main()
+{
+	vi numbers(10,0);
+	cout << "{ ";
+	for(auto i : numbers) {
+		cout << i << ", ";
+	}
+	cout << "}\n";
+
+	return 0;
+}
