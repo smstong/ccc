a8e1f0697d76420dbf0f589b1598d7e27450edd7
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2022-08-26 10:07:01 -0400

    finish 2021/j3

--- 2021/j3/j3.cpp
+++ 2021/j3/j3.cpp
@@ -3,5 +3,29 @@ using namespace std;
 
 int main()
 {
+	string line;
+	string direction;
+	int d1,d2,steps,sum;
+
+	while(true){
+		cin >> line;
+		if(line == "99999"){
+			break;
+		}
+
+		d1 = stoi(line.substr(0,1));
+		d2 = stoi(line.substr(1,1));
+		steps = stoi(line.substr(2,3));
+		sum = d1 + d2;
+		if(sum == 0){
+			// don't change direction
+		}else if(sum % 2 == 0){
+			direction = "right";
+		}else{
+			direction = "left";
+		}
+
+		cout << direction << " " << steps << "\n";
+	}
     return 0;
 }
