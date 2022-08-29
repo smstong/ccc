72a6a1dec6a98f501cfb849896016958021cb9df
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2022-08-28 20:25:18 -0700

    add 2019/j5, not finished yet.

---
+++ 2019/j5/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ls TestData/*.in |
+while read i
+do
+	f_in=$i
+	f_out=$(echo $i | sed -e 's/in$/out/')
+	output=$(cat $f_in | ./j5)
+	answer=$(cat $f_out)
+
+	echo -n "$f_in: "
+	if [[ "$output" == "$answer" ]]; then
+		echo "PASS"
+	else
+		echo "FAIL"
+	fi
+done
