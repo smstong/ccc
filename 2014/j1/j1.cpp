297f37e453564a8a48e5af908b788ae7d48f65e0
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2022-09-15 19:53:48 -0400

    finish 2014/j1

---
+++ 2014/j1/j1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a1, a2, a3;
+	cin >> a1 >> a2 >> a3;
+
+	if(a1 + a2 + a3 != 180 ){
+		cout << "Error";
+
+	}else if(a1 == a2 && a1 == a3){
+		cout << "Equilateral";
+
+	}else if(a1 == a2 || a1 == a3 || a2 == a3){
+		cout << "Isosceles";
+	}else{
+		cout << "Scalene";
+	}
+	
+	cout << "\n";
+	return 0;
+}
+
