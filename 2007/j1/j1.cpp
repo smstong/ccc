9aa9d51555c83c1182731d2cc870f0abb80a241f
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2022-11-11 14:12:37 -0500

    finish 2007/j1

---
+++ 2007/j1/j1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void sort(int a[3])
+{
+	int i=0;
+	for(i=0; i<3; i++){
+		int minIndex = i;
+		for(int j=i+1; j<3; j++){
+			if(a[j] < a[i]){
+				minIndex = j;
+			}
+		}
+		if(minIndex != i){
+			int t = a[i];
+			a[i] = a[minIndex];
+			a[minIndex] = t;
+		}
+	}
+}
+
+int main()
+{
+	int a[3];
+	cin >> a[0] >> a[1] >> a[2];
+
+	sort(a);
+	cout << a[1] << endl;
+
+	return 0;
+}
