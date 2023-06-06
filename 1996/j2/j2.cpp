0fd33259624bfcd256510905810006a714aeecf1
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2023-06-05 23:11:18 -0400

    add 1996/j2

---
+++ 1996/j2/j2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void div11(int n) {
+
+}
+int main(){
+	int N;
+	cin >> N;
+	for(int i=0; i<N; i++){
+		int n;
+		cin >> n;
+		div11(n);
+	}
+	return 0;
+}
