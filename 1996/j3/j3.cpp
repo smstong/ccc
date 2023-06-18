3b29400d643420b0a34c84d8811b3e6f35758584
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2023-06-18 01:17:58 -0400

    finish 1996/j3.

--- 1996/j3/j3.cpp
+++ 1996/j3/j3.cpp
@@ -1,6 +1,78 @@
 #include <iostream>
 using namespace std;
 
+void sub_one(string& s){
+	int n = s.length();
+
+	if(s[n-1] == '1'){
+		s[n-1] = '0';
+	}else{
+		for(int i=n-1; i>=0; i--){
+			if(s[i] == '0'){
+				s[i] = '1';
+			}else{
+				s[i] = '0';
+				break;
+			}
+		}
+	}
+}
+
+int count_ones(string s){
+	int n = s.length();
+	int sum = 0;
+	for(int i=0; i<n; i++){
+		if(s[i] == '1'){
+			sum ++;
+		}
+	}
+	return sum;
+}
+void f(int n, int k){
+	string max = "";
+	for(int i=0; i<n; i++){
+		if(i<k){
+			max += "1";
+		}else{
+			max += "0";
+		}
+	}
+
+	string min = "";
+	for(int i=0; i<n; i++){
+		if(n-i <= k){
+			min += "1";
+		}else{
+			min += "0";
+		}
+	}
+
+	cout << "The bit patterns are\n";
+	string s = max;
+	while(true){
+		if(count_ones(s) == k){
+			cout << s << endl;
+		}
+		if(s == min){
+			break;
+		}
+		sub_one(s);
+		if(s == min){
+			cout << s << endl;
+			break;
+		}
+	}
+}
+
 int main(){
+	int N;
+	cin >> N;
+	for(int i=0; i<N; i++){
+		int n,k;
+		cin >> n >> k;
+		f(n,k);
+		cout << endl;
+	}
+
 	return 0;
 }
