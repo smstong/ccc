ab90e03a34baf0976aa0bcec0613af6534ee03c0
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2023-06-18 00:18:13 -0400

    finish 1996/j2.

--- 1996/j2/j2.cpp
+++ 1996/j2/j2.cpp
@@ -1,16 +1,61 @@
 #include <iostream>
 using namespace std;
 
-void div11(int n) {
+// substraction
+void sub(string& s, char digit){
+	int borrow = 0;
+	int n = s.length();
+	for(int i=n-1; i>=0; i--){
+		int d_i = s[i]-'0';
+
+		int d = 0;
+		if(i == n-1){
+			d = digit-'0';
+		}
+
+		int d_r = d_i - borrow - d;
+		if(d_r < 0){
+			d_r += 10;
+			borrow = 1;
+		}else{
+			borrow = 0;
+		}
+		s[i] = d_r + '0';
+		if(borrow == 0){
+			break;
+		}
+	}
+	if(s[0] == '0'){
+		s = s.substr(1);
+	}
+}
+
+void div11(string s) {
+	string old = s;
+	cout << s << endl;
+	int len = s.length();
+	while(len > 2){
+		char digit = s[len-1];
+		s = s.substr(0, len-1);
+		sub(s, digit);
+		cout << s << endl;
+		len = s.length();
+	}
+	if(s.length() == 2 && s[0] == s[1]){
+		cout << "The number " << old << " is divisible by 11.\n";
+	}else{
+		cout << "The number " << old << " is not divisible by 11.\n";
+	}
 
 }
 int main(){
 	int N;
 	cin >> N;
 	for(int i=0; i<N; i++){
-		int n;
-		cin >> n;
-		div11(n);
+		string s;
+		cin >> s;
+		div11(s);
+		cout << endl;
 	}
 	return 0;
 }
